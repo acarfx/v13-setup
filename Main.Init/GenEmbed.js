04bfa99fe43a4687ec15c13bebed362cd4433cc7
acar <77089894+acarfx@users.noreply.github.com>
2022-06-16 22:47:17 +0300

    Update GenEmbed.js

--- Main.Init/GenEmbed.js
+++ Main.Init/GenEmbed.js
@@ -3,14 +3,7 @@ const {MessageEmbed, Message, Util, Client} = require('discord.js')
 class genEmbed extends MessageEmbed {
     constructor(...opt) {
         super(opt)
-        let guild = client.guilds.cache.get(sistem.sunucuID)
-        if(guild) {
-            this.setAuthor({name: guild.name, iconURL: guild.iconURL({dynamic: true})})
-            this.setColor("RANDOM")
-        } else {
-            this.setColor("RANDOM")
-        }
-        
+        this.setColor("RANDOM")
     }
 
   setAuthor(options, deprecatedIconURL, deprecatedURL) {
